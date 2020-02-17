aa74c67731d4717766bc3cf6a969682a8b97f0cb
antoniotm22 <55539288+antoniotm22@users.noreply.github.com>
2020-02-17 09:01:03 +0100

    Primer Commit

---
+++ src/operar_arrays/arrays.java
@@ -0,0 +1,57 @@
+package operar_arrays;
+
+public class arrays {
+
+	
+public static void recorrerAray() {
+	int array[]= new int [10];
+	for (int i = 0; i < array.length; i++) {
+		array[i] = i + 1;
+		
+	}	
+	for (int i = 0; i < array.length; i++) {
+		System.out.print(array[i]+"/n");
+	}
+	System.out.println();
+}
+
+public static void imprimeArray() {
+	int array[]= new int[10];
+	for (int i = 0; i < array.length; i++) {
+		System.out.print(array[i]+"/n");
+	}
+	System.out.println();
+}
+
+public static void rellenar_array() {
+	
+	int array[]= new int [10];
+	for(int i =0; i<array.length; i++) {
+		array[i] = (int)Math.round(Math.random()*(20-1)+1);	
+		System.out.println(array);
+	}
+}
+
+public static void sumarArray() {
+	int array[]= new int[10];
+	int total=0;
+	int i;
+	
+	for ( i = 0; i < array.length; i++) {
+		array[i] = i + 1;		
+	}
+	total += array[i];
+	System.out.println(total);
+}
+
+
+	
+	
+	public static void main(String[] args) {
+	recorrerAray();
+	
+	}
+	
+	
+
+}
